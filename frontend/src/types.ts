ff4f555d53b283d87648738f04e44f7cb8ccc89c
Rakesh Mahato <rmahato64@gmail.com>
2021-10-02 21:23:53 +0300

    Added the data types required by application

---
+++ frontend/src/types.ts
@@ -0,0 +1,8 @@
+export type Data = {
+    date: string;
+    sensor1: string;
+    sensor2: string;
+    sensor3: string;
+    sensor4: string;
+}
+export const SENSOR_KEYS = ['sensor1', 'sensor2', 'sensor3', 'sensor4'];
