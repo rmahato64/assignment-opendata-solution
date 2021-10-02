7f892bbc0efdb8b35001a228574713aa491bfa47
Rakesh Mahato <rmahato64@gmail.com>
2021-10-02 21:52:31 +0300

    Fetch data from backend using react-fetch-hook

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import useFetch from 'react-fetch-hook';
 import logo from './logo.svg';
 import './App.css';
+import { Data } from './types';
 
-function App() {
+const API_URL = 'https://winged-odyssey-244910.ew.r.appspot.com/';
+
+const App = (): React.ReactElement => {
+  const { isLoading, data, error } = useFetch<Data[]>(API_URL);
+  console.log(isLoading, data);
+  if (error?.message) {
+    return <div>{error?.message}</div>;
+  }
   return (
     <div className="App">
       <header className="App-header">
